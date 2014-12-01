0e7f468d8a84f1ad7a2054391f3b55372b4746a6
unknown <trupti.pawar@Truptinew.wits.local>
2014-12-01 19:59:33 +0530

    master

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['TestPro2'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'bfc7fd0bf81777846ab4eba0db861f29b79118fc',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
